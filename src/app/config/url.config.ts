f7ff83d95bd8851d30d052697b3f809f35576ac8
mohamedikbel <56933710+mohamedikbel@users.noreply.github.com>
2020-08-12 14:15:44 +0200

    Update url.config.ts
    change api link

--- src/app/config/url.config.ts
+++ src/app/config/url.config.ts
@@ -1,8 +1,8 @@
 export const API_URLS ={
 
-TRANSACTIONS_URL :'http://localhost:8086/api/account/retrieve-transactions',
-CompConF_Url :'http://localhost:8086/api/account/loaddata',
-USr_URL:'http://localhost:8086/api/user'
+TRANSACTIONS_URL :'https://wifak-server.herokuapp.com/api/account/retrieve-transactions',
+CompConF_Url :'https://wifak-server.herokuapp.com/api/account/loaddata',
+USr_URL:'https://wifak-server.herokuapp.com/api/user'
 
 
 };

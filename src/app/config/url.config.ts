a4d58eeb374f43f1d9c606631491aa047c12de7d
mohamedikbel <56933710+mohamedikbel@users.noreply.github.com>
2020-08-24 02:34:33 +0200

    fix endpoints name

--- src/app/config/url.config.ts
+++ src/app/config/url.config.ts
@@ -1,7 +1,7 @@
 export const API_URLS ={
 
-TRANSACTIONS_URL :'https://wifak-server.herokuapp.com/api/account/retrieve-transactions',
-CompConF_Url :'https://wifak-server.herokuapp.com/api/account/loaddata',
+TRANSACTIONS_URL :'https://wifak-server.herokuapp.com/api/transactions',
+CompConF_Url :'https://wifak-server.herokuapp.com/api/transactions',
 USr_URL:'https://wifak-server.herokuapp.com/api/user'
 
 
